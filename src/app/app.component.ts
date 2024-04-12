ddc71fe93a10d91789574512fa6dfa4128074e19
unknown <danilo.cavalli@enel.com>
2024-04-12 14:38:52 +0200

    init store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosStore } from './store/todos.store';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'toDoListApp';
+
+  store = inject(TodosStore);
+
+  constructor(){
+    this.store.todos()
+  }
+
 }
---
+++ src/app/model/mock-data.ts
@@ -0,0 +1,8 @@
+import { Todo } from "./todo.model";
+
+export const TODOS: Todo[] = [
+    { id: '1', title: 'angular course', completed: true },
+    { id: '2', title: 'react course', completed: true },
+    { id: '3', title: 'python course', completed: false },
+    { id: '4', title: 'java course', completed: false },
+]
